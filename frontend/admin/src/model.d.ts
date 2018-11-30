56dee5083f0f17971d3da9b81bd6ad8cecc703ee
gsalpha <yuzhe5671@gmail.com>
2018-11-30 17:58:23 +0800

    add login saga start

---
+++ frontend/admin/src/model.d.ts
@@ -0,0 +1,8 @@
+declare namespace MODEL {
+    export interface IAuth {
+        token: string
+        error: string
+        username: string
+        loggingIn: boolean
+    }
+}
--- frontend/admin/src/store/auth/actions.ts
+++ frontend/admin/src/store/auth/actions.ts
@@ -1,2 +1,33 @@
-import { action } from 'typesafe-actions'
-import * as constants from './constants'
+import constants from './constants'
+
+export interface ILoginAction {
+    type: constants.LOGIN_REQUEST
+    payload: API.auth.ILoginReq
+}
+
+export interface ILoginSuccessAction {
+    type: constants.LOGIN_SUCCESS
+    payload: API.auth.ILoginRes
+}
+
+export interface ILoginFailureAction {
+    type: constants.LOGIN_FAILURE
+    payload: string
+}
+
+export const login = (payload: API.auth.ILoginReq): ILoginAction => ({
+    type: constants.LOGIN_REQUEST,
+    payload
+})
+
+export const loginSuccess = (
+    payload: API.auth.ILoginRes
+): ILoginSuccessAction => ({
+    type: constants.LOGIN_SUCCESS,
+    payload
+})
+
+export const loginFailure = (err: API.error): ILoginFailureAction => ({
+    type: constants.LOGIN_FAILURE,
+    payload: err.message
+})
